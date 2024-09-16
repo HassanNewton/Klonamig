177b20cda7e4fda66e712a54aa52022d85e8318f
HassanNewton <hassan.hussin@newton.se>
2024-09-16 10:16:47 +0200

    Add project files.

---
+++ Klonamig/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Klonamig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        static void printName()
+        {
+            //gör om ¨så att metoden ska returna ditt namn skriv sedan ut det i main
+        }
+    }
+}
